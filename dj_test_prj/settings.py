2896e24963be0b21b2b4a1e1266bcbd4551dd54b
gleb-r <rad.gleb@gmail.com>
2017-04-05 21:02:37 +0300

    2 day changes

--- dj_test_prj/settings.py
+++ dj_test_prj/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #some update
+    #another update
 ]
 
 ROOT_URLCONF = 'dj_test_prj.urls'
